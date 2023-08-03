9f35a1f0b4d91b5665aa536c11e83d2658c58b26
Andrey <A.com>
2023-08-03 22:04:59 +0300

    сумма 2 рандомных чисел

--- Ex1/Sum/Program.cs
+++ Ex1/Sum/Program.cs
@@ -1,3 +1,6 @@
-﻿int numberA = 3;
-int numberB = 5;
+﻿int numberA = new Random().Next(1,10);  //1 2 3 4 5 6 7 8 9 
+System.Console.WriteLine(numberA);
+int numberB = new Random().Next(1,10);
+System.Console.WriteLine(numberB);
 System.Console.WriteLine(numberA + numberB);
+
