a2a8db470cf91efa89a2b470550f3f81daa78564
Andrey <A.com>
2023-08-03 22:19:10 +0300

    ifelse

---
+++ Ex1/IfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Как Вас зовут?");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "маша")
+{
+    System.Console.WriteLine("Ура, это же МАША!!!");
+}
+else
+{
+    System.Console.Write("Привет, ");
+    System.Console.Write(username);
+}
